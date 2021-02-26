1e8ff3561b018627cb8dfb80436f65d0739ec678
TXLuong <tranxuanluong98@gmail.com>
2021-02-26 13:15:22 +0700

    route

---
+++ systemui/src/routers/workRouter.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Switch, useRouteMatch } from "react-router";
+import Recents from '../components/recents';
+export default function Workrouter() {
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <Switch>
+        <Route component = {Recents} path = {"/recents"}></Route>
+      </Switch>
+    </div>
+  );
+}
+
+// from App -> Routes -> MainAppRoute -> BacklogRoute 
